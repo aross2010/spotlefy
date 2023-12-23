35b08f9f731765bafac5eea752b87d56d0416c92
Alex Ross <adross1027@gmail.com>
2023-12-23 14:53:46 -0800

    #

--- app/context/acess-token-context.tsx
+++ app/context/acess-token-context.tsx
@@ -26,6 +26,7 @@ export default function AccessTokenContextProvider({
     try {
       const res = await axios.get('/api/access_token')
       const token = res.data
+      console.log('context token', token)
       setAccessToken(
         'BQCeYERAOsXYYxmFxZNfcH7_sAIYsuvlqxfxmMRE1fqA6uvDnMEA5Ij_ZTKyz79r3czdy-wrWMf29oG4jIaqlf9lf4btNIZ8Ev7Ch-uBB3EiqqauOhFli16fJPZ6hlW5ZjxPtrrsEDS9ImgnQ-x59AS7S-QuGeS11v2O4oBp_W4c9oxM9ynoxvs6LhDGZfWwxQ'
       )
