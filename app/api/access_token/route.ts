38d347e69958037aaa480bd9bd10125d05ddb0f2
Alex Ross <adross1027@gmail.com>
2023-12-23 15:54:18 -0800

    #

--- app/api/access_token/route.ts
+++ app/api/access_token/route.ts
@@ -7,18 +7,20 @@ export async function GET() {
     refresh_token: process.env.SPOTIFY_REFRESH_TOKEN as string,
   }
   try {
-    const res = await axios.post(
-      'https://accounts.spotify.com/api/token',
-      new URLSearchParams(params).toString(),
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Authorization: `Basic ${process.env.SPOTIFY_AUTHORIZATION as string}`,
-        },
-      }
-    )
-    console.log('fetchded new token', res.data.access_token)
-    return NextResponse.json(res.data.access_token)
+    const res = await fetch('https://accounts.spotify.com/api/token', {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams(params).toString(),
+    })
+    const data = await res.json()
+    console.log('in route', data.access_token)
+    return NextResponse.json(data.access_token)
   } catch (err) {
     return NextResponse.json({
       error: 'Something went wrong fetching the access token.',
