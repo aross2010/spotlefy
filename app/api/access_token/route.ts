487f2c51809a480ba479f680fbbe668eac0daecf
Alex Ross <adross1027@gmail.com>
2023-12-23 15:27:43 -0800

    #

--- app/api/access_token/route.ts
+++ app/api/access_token/route.ts
@@ -19,10 +19,11 @@ export async function GET() {
         },
       }
     )
-    console.log('INDIDE ROUTE', res.data.access_token)
-    return NextResponse.json(res.data.access_token)
+    console.log('token fetched in route but not used:', res.data.access_token)
+    return NextResponse.json(
+      'BQCeYERAOsXYYxmFxZNfcH7_sAIYsuvlqxfxmMRE1fqA6uvDnMEA5Ij_ZTKyz79r3czdy-wrWMf29oG4jIaqlf9lf4btNIZ8Ev7Ch-uBB3EiqqauOhFli16fJPZ6hlW5ZjxPtrrsEDS9ImgnQ-x59AS7S-QuGeS11v2O4oBp_W4c9oxM9ynoxvs6LhDGZfWwxQ'
+    )
   } catch (err) {
-    console.log(err)
     return NextResponse.json({
       error: 'Something went wrong fetching the access token.',
     })
--- app/context/acess-token-context.tsx
+++ app/context/acess-token-context.tsx
@@ -26,9 +26,8 @@ export default function AccessTokenContextProvider({
     try {
       const res = await axios.get('/api/access_token')
       const token = res.data
-      setAccessToken(
-        'BQCeYERAOsXYYxmFxZNfcH7_sAIYsuvlqxfxmMRE1fqA6uvDnMEA5Ij_ZTKyz79r3czdy-wrWMf29oG4jIaqlf9lf4btNIZ8Ev7Ch-uBB3EiqqauOhFli16fJPZ6hlW5ZjxPtrrsEDS9ImgnQ-x59AS7S-QuGeS11v2O4oBp_W4c9oxM9ynoxvs6LhDGZfWwxQ'
-      )
+      console.log('fetched this token inside context:', token)
+      setAccessToken(token)
     } catch (err) {}
   }
 
@@ -45,10 +44,6 @@ export default function AccessTokenContextProvider({
       clearInterval(tokenRefreshInterval)
     }
   }, [])
-
-  useEffect(() => {
-    console.log('access token', accessToken)
-  }, [accessToken])
 
   return (
     <AccessTokenContext.Provider
