aefd420e51d82c58ae05e61bcc543776c759dca1
Alex Ross <adross1027@gmail.com>
2023-12-29 20:50:39 -0800

    #

--- app/functions/fetch-audio-preview.tsx
+++ app/functions/fetch-audio-preview.tsx
@@ -1,14 +1,20 @@
+'use client'
 import axios from 'axios'
 import { useAccessToken } from '../context/acess-token-context'
 
 export const fetchAudioPreview = async (trackUrl: string, token: string) => {
   if (!trackUrl) return null
 
-  const res = await axios.get(trackUrl, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  try {
+    const res = await axios.get(trackUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
 
-  return res.data.preview_url
+    return res.data.preview_url
+  } catch (err: any) {
+    console.log('Error Here', err)
+    return null
+  }
 }
