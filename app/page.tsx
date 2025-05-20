0616ff2a9bf5d6e9350accec554b1ed27a6ecc8d
Alex Ross <adross1027@gmail.com>
2025-05-20 11:02:17 -0700

    #

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,16 @@ export default function Home() {
   return (
     <Fragment>
       <section className="flex flex-col items-center py-12 min-h-screen">
-        <h1 className="font-bold md:text-4xl text-xl flex items-center gap-2 pb-4 tracking-tight  text-green-500 ">
+        <h1 className="font-bold md:text-4xl text-3xl flex items-center gap-2 pb-4 tracking-tight  text-green-500 ">
           Spotlefy <FaSpotify />
         </h1>
-        {/* <h5 className="text-gray-400 font-medium text-center">
-          Customize your own Heardle game via Spotify.
-        </h5> */}
+
         <div className="mt-6 w-full flex flex-col items-center justify-center">
           <SearchTypeSelector disabled />
         </div>
-        <p className="text-gray-400 text-center font-medium mt-4">
+        <p className="text-gray-400 text-center sm:text-base text-sm mt-4">
           Due to recent Spotify API changes, Spotlefy is no longer available.
-          <br />
-          Check out the project on{' '}
+          View the project on{' '}
           <Link
             className="underline"
             href={'https://github.com/aross2010/spotlefy'}
